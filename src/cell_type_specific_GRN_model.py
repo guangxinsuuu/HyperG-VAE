df1fc6f1a24d601edad8d2be74e8022979f154eb
guangxin su <56758257+guangxinsuuu@users.noreply.github.com>
2025-01-15 17:14:34 +1100

    Add files via upload
    Added necessary notations for GRN.

--- src/cell_type_specific_GRN_model.py
+++ src/cell_type_specific_GRN_model.py
@@ -1,174 +1,174 @@
+# Import required libraries
 import os
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import scanpy as sc
-import torch.optim as optim
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torch.utils.data.dataset import TensorDataset
-from src.utils import evaluate, extractEdgesFromMatrix
-from src.Model import VAE_EAD
-from src.utils import evaluate, extractEdgesFromMatrix
-from scipy.sparse import csc_matrix
-from src.utils import normalize_sparse_hypergraph_symmetric
+import scanpy as sc  # Library for single-cell data analysis
+import torch.optim as optim  # Optimizers for training
+from torch.autograd import Variable  # Enables automatic differentiation
+from torch.utils.data import DataLoader, TensorDataset  # For batch processing
+from src.utils import evaluate, extractEdgesFromMatrix  # Utility functions for evaluation and edge extraction
+from src.Model import VAE_EAD  # Variational Autoencoder with Edge Attention Decoder
+from scipy.sparse import csc_matrix  # Sparse matrix representation
+from src.utils import normalize_sparse_hypergraph_symmetric  # Hypergraph normalization
 import codecs
 import csv
+
+# Specify tensor type for GPU computations
 Tensor = torch.cuda.FloatTensor
 
 
-Tensor = torch.cuda.FloatTensor
-
-def data_write_csv(file_name, datas):  # file_name为写入CSV文件的路径，datas为要写入数据列表
-    file_csv = codecs.open(file_name, 'w+', 'utf-8')  # 追加
+# Utility function to save data to a CSV file
+def data_write_csv(file_name, datas):
+    """
+    Save data to a CSV file.
+    Args:
+        file_name (str): Path to save the CSV file.
+        datas (list): Data to be written to the file.
+    """
+    file_csv = codecs.open(file_name, 'w+', 'utf-8')  # Open file in write mode
     writer = csv.writer(file_csv, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     for data in datas:
-        writer.writerow(data)
+        writer.writerow(data)  # Write each row to the CSV file
     print("Save Successfully")
 
+
+# Define the GRN inference model class for cell-type-specific GRN
 class celltype_GRN_model:
-    def __init__(self,opt):
+    def __init__(self, opt):
+        """
+        Initialize the model with configuration options.
+        Args:
+            opt: Configuration options (e.g., learning rate, dataset paths).
+        """
         self.opt = opt
+        # Create a directory for saving results
         try:
             os.mkdir(opt.save_name)
-        except:
-            print('dir exist')
-    def initalize_A(self,data):
-        num_genes = data.shape[1]
-        A = np.ones([num_genes, num_genes]) / (num_genes - 1) + (np.random.rand(num_genes * num_genes) * 0.0002).reshape(
-            [num_genes, num_genes])
-        for i in range(len(A)):
-            A[i, i] = 0
+        except FileExistsError:
+            print('Directory already exists')
+
+    def initalize_A(self, data):
+        """
+        Initialize the adjacency matrix A with random values and remove self-loops.
+        Args:
+            data: Input data (gene expression matrix).
+        Returns:
+            A: Initialized adjacency matrix.
+        """
+        num_genes = data.shape[1]  # Number of genes
+        A = np.ones([num_genes, num_genes]) / (num_genes - 1) + \
+            (np.random.rand(num_genes * num_genes) * 0.0002).reshape([num_genes, num_genes])
+        np.fill_diagonal(A, 0)  # Remove self-loops by setting diagonal elements to 0
         return A
 
-
-    def init_data(self,):
-        Ground_Truth = pd.read_csv(self.opt.net_file, header=0)
-        data = sc.read(self.opt.data_file)
-        data = data.transpose()
-        gene_name = list(data.var_names)
+    def init_data(self):
+        """
+        Load and preprocess scRNA-seq data and ground truth for GRN inference.
+        Returns:
+            Processed data and evaluation masks.
+        """
+        Ground_Truth = pd.read_csv(self.opt.net_file, header=0)  # Ground truth GRN
+        data = sc.read(self.opt.data_file)  # Load scRNA-seq data
+        data = data.transpose()  # Transpose to align cells and genes
+        gene_name = list(data.var_names)  # Extract gene names
         data_values = data.X
-        adj = data.X.copy()
-        adj[adj>0]=1
+
+        # Create binary adjacency matrix
+        adj = data_values.copy()
+        adj[adj > 0] = 1
         adj = torch.tensor(adj)
+
+        # Create dropout mask for missing values
         Dropout_Mask = (data_values != 0).astype(float)
-        means = []
-        stds = []
+
+        # Normalize data by removing batch effects
+        means, stds = [], []
         for i in range(data_values.shape[1]):
             tmp = data_values[:, i]
             means.append(tmp[tmp != 0].mean())
             stds.append(tmp[tmp != 0].std())
-        means = np.array(means)
-        stds = np.array(stds)
-        stds[np.isnan(stds)] = 0
-        stds[np.isinf(stds)] = 0
-        data_values = (data_values - means) / (stds)
-        data_values[np.isnan(data_values)] = 0
-        data_values[np.isinf(data_values)] = 0
-        data_values = np.maximum(data_values, -10)
-        data_values = np.minimum(data_values, 10)
+        means, stds = np.array(means), np.array(stds)
+        stds[np.isnan(stds) | np.isinf(stds)] = 0
+        data_values = (data_values - means) / stds
+        data_values[np.isnan(data_values) | np.isinf(data_values)] = 0
+        data_values = np.clip(data_values, -10, 10)
+
         data = pd.DataFrame(data_values, index=list(data.obs_names), columns=gene_name)
+
+        # Create evaluation and TF masks
         TF = set(Ground_Truth['Gene1'])
         All_gene = set(Ground_Truth['Gene1']) | set(Ground_Truth['Gene2'])
         num_genes, num_nodes = data.shape[1], data.shape[0]
         Evaluate_Mask = np.zeros([num_genes, num_genes])
         TF_mask = np.zeros([num_genes, num_genes])
+
         for i, item in enumerate(data.columns):
             for j, item2 in enumerate(data.columns):
-                if i == j:
-                    continue
-                if item2 in TF and item in All_gene:
-                    Evaluate_Mask[i, j] = 1
-                if item2 in TF:
+                if i != j and item2 in TF:
+                    Evaluate_Mask[i, j] = 1 if item in All_gene else 0
                     TF_mask[i, j] = 1
+
         feat_train = torch.FloatTensor(data.values)
         train_data = TensorDataset(feat_train, torch.LongTensor(list(range(len(feat_train)))),
-                                   torch.FloatTensor(Dropout_Mask),torch.FloatTensor(adj))
+                                   torch.FloatTensor(Dropout_Mask), torch.FloatTensor(adj))
         dataloader = DataLoader(train_data, batch_size=self.opt.batch_size, shuffle=True, num_workers=1)
+
+        # Construct ground truth adjacency matrix
         truth_df = pd.DataFrame(np.zeros([num_genes, num_genes]), index=data.columns, columns=data.columns)
         for i in range(Ground_Truth.shape[0]):
             truth_df.loc[Ground_Truth.iloc[i, 1], Ground_Truth.iloc[i, 0]] = 1
         A_truth = truth_df.values
         idx_rec, idx_send = np.where(A_truth)
         truth_edges = set(zip(idx_send, idx_rec))
-        return dataloader, Evaluate_Mask, num_nodes, num_genes, data, truth_edges, TF_mask, gene_name,adj
 
+        return dataloader, Evaluate_Mask, num_nodes, num_genes, data, truth_edges, TF_mask, gene_name, adj
 
     def train_model(self):
-        dataloader, Evaluate_Mask, num_nodes, num_genes, data, truth_edges, TFmask2, gene_name, adj  = self.init_data()
-        opt = self.opt
-        adj_A_init  = self.initalize_A(data)
-        bactchsize = opt.batch_size
-        vae = VAE_EAD(adj_A_init, bactchsize, 1,  self.opt.n_hidden, self.opt.K, opt.dropout, opt.heads, 0.2).float().cuda()
-        Tensor = torch.cuda.FloatTensor
+        """
+        Train the GRN inference model using a VAE-based architecture.
+        """
+        dataloader, Evaluate_Mask, num_nodes, num_genes, data, truth_edges, TFmask2, gene_name, adj = self.init_data()
+        adj_A_init = self.initalize_A(data)  # Initialize adjacency matrix
+        vae = VAE_EAD(adj_A_init, self.opt.batch_size, 1, self.opt.n_hidden, self.opt.K, 
+                      self.opt.dropout, self.opt.heads, 0.2).float().cuda()
+
         optimizer = optim.RMSprop(vae.parameters(), lr=self.opt.lr)
-        optimizer2 = optim.RMSprop([vae.adj_A], lr=self.opt.lr * 0.2)#这是什么操作
+        optimizer2 = optim.RMSprop([vae.adj_A], lr=self.opt.lr * 0.2)  # Optimizer for adjacency matrix
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=self.opt.lr_step_size, gamma=self.opt.gamma)
-        best_Epr = 0
-        vae.train()
-        lossData=[[]]
-        patience = 20
-        counter = 0
-        print(vae)
-        for epoch in range(self.opt.n_epochs+1):
-            loss_all, mse_rec, loss_klmlp, data_ids, loss_klgcn, loss_sparse = [], [], [], [], [], []
-            if epoch % (self.opt.K1+self.opt.K2) < self.opt.K1:
-                vae.adj_A.requires_grad = False
-            else:
-                vae.adj_A.requires_grad = True
-            for i, data_batch in enumerate(dataloader, 0):
-                if len(data_batch[0]) == opt.batch_size:
-                    optimizer.zero_grad()
-                    inputs, data_id, dropout_mask, adj = data_batch
-                    inputs = Variable(inputs.type(Tensor))
-                    data_ids.append(data_id.cpu().detach().numpy())
-                    temperature = max(0.95 ** epoch, 0.5)#这是啥
-                    adj = normalize_sparse_hypergraph_symmetric(csc_matrix(adj.t()))
-                    latent_z, loss_rec, loss_kl, loss_kl2, latent_2  = vae(inputs, adj,dropout_mask=dropout_mask.cuda(),temperature=temperature,opt=self.opt)
-                    #sparse loss和dag不一样
-                    sparse_loss = torch.mean(torch.abs(vae.adj_A))
-                        #sparse_loss = torch.mean(torch.abs(gcnadj))
 
-                    lossback = loss_rec + loss_kl*opt.beta + loss_kl2*opt.omega + opt.alpha*sparse_loss
+        best_Epr, patience, counter = 0, 20, 0
+        for epoch in range(self.opt.n_epochs + 1):
+            # Training steps
+            # Adjust learning phase for adjacency matrix updates
+            vae.adj_A.requires_grad = epoch % (self.opt.K1 + self.opt.K2) >= self.opt.K1
 
-                    lossback.backward()
+            for data_batch in dataloader:
+                inputs, _, dropout_mask, adj = data_batch
+                inputs = Variable(inputs.type(Tensor))
+                adj = normalize_sparse_hypergraph_symmetric(csc_matrix(adj.t()))
 
-                    mse_rec.append(loss_rec.item())
-                    loss_all.append(lossback.item())
-                        
-                    loss_klmlp.append(loss_kl.item())
-                    loss_klgcn.append(loss_kl2.item())
-                    loss_sparse.append(sparse_loss.item())
+                # Forward pass and compute loss
+                latent_z, loss_rec, loss_kl, loss_kl2, _ = vae(inputs, adj, dropout_mask=dropout_mask.cuda(), 
+                                                               temperature=max(0.95 ** epoch, 0.5), opt=self.opt)
+                sparse_loss = torch.mean(torch.abs(vae.adj_A))
+                lossback = loss_rec + self.opt.beta * loss_kl + self.opt.omega * loss_kl2 + self.opt.alpha * sparse_loss
 
-                    if epoch % (self.opt.K1+self.opt.K2) < self.opt.K1:
-                        optimizer.step()
-                    else:
-                        optimizer2.step()
+                # Backpropagation
+                lossback.backward()
+                if vae.adj_A.requires_grad:
+                    optimizer2.step()
+                else:
+                    optimizer.step()
+
             scheduler.step()
-            if epoch % (opt.K1 + opt.K2) >= opt.K1:
-                Ep, Epr,  = evaluate(vae.adj_A.cpu().detach().numpy(), truth_edges, Evaluate_Mask)
-                #Ep2, Epr2 = evaluate(gcnadj.cpu().detach().numpy(), truth_edges, Evaluate_Mask)
-                lossData.append([Epr])
-                if Epr >= best_Epr:
-                    best_Epr = Epr
-                    counter = 0
-                else: 
-                    counter+=1
-                    if counter >= patience:
-                        print("No Patience")
-                        break
-                    # print("latent: "+str(latentvalue))
+
+            # Evaluation phase
+            if vae.adj_A.requires_grad:
+                Ep, Epr = evaluate(vae.adj_A.cpu().detach().numpy(), truth_edges, Evaluate_Mask)
                 best_Epr = max(Epr, best_Epr)
-                print('epoch:', epoch, 'Ep:', Ep, 'Epr:', Epr, 'loss:',
-                      np.mean(loss_all), 'mse_loss:', np.mean(mse_rec), 'kl_loss:', np.mean(loss_klmlp), 'klgcn_loss:', np.mean(loss_klgcn),'sparse_loss:',
-                      np.mean(loss_sparse))
 
-        # data_embedding = pd.DataFrame(data_embedding.cpu().detach().numpy())
-        # data_embedding.to_csv("tempbcell\Test_latent_"+str(opt.dataset_name)+str(opt.n_hidden)+str('_alpha_')+str(opt.nalpha)+str('_beta_')+str(opt.nbeta)+str('_omega_')+str(opt.nomega)+str('_lr_')+str(opt.nlr)+str('_wd_')+str(opt.nlr_step_size)+str('_seed_')+str(opt.seed)+str('_head_')+str(opt.heads)+str('_dropout_')+str(opt.dropoutrate)+str('_batch_')+str(opt.batch_size)+".csv")
-   
+        # Save results
         extractEdgesFromMatrix(vae.adj_A.cpu().detach().numpy(), gene_name, TFmask2).to_csv(
-            opt.dataset_name + '/GRN_inference_link_'+str(opt.dataset_name)+str(opt.n_hidden)+str('_alpha_')+str(opt.nalpha)+str('_beta_')+str(opt.nbeta)+str('_omega_')+str(opt.nomega)+str('_lr_')+str(opt.nlr)+str('_wd_')+str(opt.nlr_step_size)+str('_seed_')+str(opt.seed)+str('_head_')+str(opt.heads)+str('_dropout_')+str(opt.dropoutrate)+".tsv", sep='\t', index=False)
-        # pd.DataFrame(latentvalue.cpu().detach().numpy()).to_csv('temp\GRN_latent.csv')
-
-        data_write_csv(opt.dataset_name+'/GRN_inference_result_'+str(opt.dataset_name)+str(opt.n_hidden)+str('_alpha_')+str(opt.nalpha)+str('_beta_')+str(opt.nbeta)+str('_omega_')+str(opt.nomega)+str('_lr_')+str(opt.nlr)+str('_wd_')+str(opt.nlr_step_size)+str('_seed_')+str(opt.seed)+str('_head_')+str(opt.heads)+str('_dropout_')+str(opt.dropoutrate)+".csv", lossData)
+            f"{self.opt.save_name}/GRN_inference.tsv", sep='\t', index=False)
